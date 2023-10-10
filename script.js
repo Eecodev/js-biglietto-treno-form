b86a0c9ac6d6dd5aef2118f262fa3538de3e16cc
Eecodev <mnasser.bbk@gmail.com>
2023-10-10 17:54:25 +0200

    price tickets calculated

--- script.js
+++ script.js
@@ -19,28 +19,21 @@ button.addEventListener('click', function(){
     const Age = document.querySelector('.insert-age').value;
     console.log(Age);
 
-if (km === '' || age === ''){
+if (Km === '' || Age === ''){
     alert('devi inserire due valori !' )
 } else {
-    console.log(km);
-    console.log(age);
+    console.log(Km);
+    console.log(Age);
 }
-const kmPrice = 0.21;
-const discountUnder18 = 20;
-const discountOver65 = 40;
 
-let km, age, price;
-let message = 'Il prezzo del tuo biglietto è: ';
+let price = Km * 0.21;
 
-if(isNaN(km) || isNaN(age)){
-    location.reload();
-}
-price = kmPrice * km;
-
-if(age < 18){
-    price = price - (price * discountUnder18/100);
-} else if(age >= 65){
-    price = price - (price * discountOver65/100);
+if ( Age === 'under-18' ) {
+    price = price - (price * 20/100); 
+} else if ( Age === 'over-65' ) {
+    price = price - (price * 40/100);
+} else (Age === 'over-18') {
+    console.log(price);
 }
 })
 
