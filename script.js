d4b19cdbfbf90149d475e1e5f88b4306634fbf79
Eecodev <mnasser.bbk@gmail.com>
2023-10-10 16:37:17 +0200

    new constants decleared

--- script.js
+++ script.js
@@ -6,31 +6,16 @@
 //va applicato uno sconto del 20% per i minorenni
 //va applicato uno sconto del 40% per gli over 65.
 
-const kmPrice = 0.21;
-const discountUnder18 = 20;
-const discountOver65 = 40;
+const km = document.querySelector('.insert-km');
+console.log(km);
 
-let km, age, price;
-let message = 'Il prezzo del tuo biglietto è: ';
+const age = document.querySelector('.insert-age');
+console.log(age)
 
-km = parseInt(prompt('Quanti km vuoi percorrere ?'));
-age = parseInt(prompt('Quanti anni hai ?'));
-
-if(isNaN(km) || isNaN(age)){
-    location.reload();
-}
-
-// console.log(km, age);
-price = kmPrice * km;
-
-// console.log(price);
-
-if(age < 18){
-    price = price - (price * discountUnder18/100);
-} else if(age >= 65){
-    price = price - (price * discountOver65/100);
-}
-
-// console.log(price);
+const button = document.querySelector('.submit');
+console.log(button);
 
 
+button.addEventListener('click', function(){
+    
+})
