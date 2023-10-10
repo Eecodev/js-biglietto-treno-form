f549db7da5156bf8486fd204d945346edf95fe84
Eecodev <mnasser.bbk@gmail.com>
2023-10-10 17:05:06 +0200

    added values to consts km and age

--- script.js
+++ script.js
@@ -6,16 +6,24 @@
 //va applicato uno sconto del 20% per i minorenni
 //va applicato uno sconto del 40% per gli over 65.
 
-const km = document.querySelector('.insert-km');
-console.log(km);
 
-const age = document.querySelector('.insert-age');
-console.log(age)
 
 const button = document.querySelector('.submit');
 console.log(button);
 
 
 button.addEventListener('click', function(){
-    
+    const km = document.querySelector('.insert-km').value;
+    console.log(km);
+
+    const age = document.querySelector('.insert-age').value;
+    console.log(age)
+
+if (km === '' || age === ''){
+    alert('devi inserire due valori !' )
+} else {
+    console.log(km);
+    console.log(age);
+}
 })
+
